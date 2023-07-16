b8978e319c675d33a7f8612c98f350780240a822
muqees99 <muqees465@gmail.com>
2023-07-16 20:37:03 +0500

    display a pattern like a right angle triangle using an asterisk using loop

--- q11.dart
+++ q11.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 void main() {
   int rows = 5; // Number of rows in the triangle
 
-  for (int i = 1; i <= rows; i++) {
+  for (int i = 1; i < rows; i++) {
     for (int j = 1; j <= i; j++) {
-      print('*');
+      stdout.write('*');
     }
-    print('\n');
+    print('');
   }
 }
